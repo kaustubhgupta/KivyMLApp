1be9a7d7722e307c49a52daf58c1c250ba319430
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-10-31 23:58:20 +0530

    all fields added:

--- app files/main.py
+++ app files/main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
+from kivy.network.urlrequest import UrlRequest
 
 Window.size = (400,600)
 
@@ -21,38 +22,93 @@ ScreenManager:
         pos_hint: {'center_y':0.75}
 
     MDTextField:
-        hint_text: '(0.0000009491-0.99579650)'
+        id: input_1
+        hint_text: '(0.0000009491 - 0.9957965)'
         width: 100
         size_hint_x: None
         pos_hint: {'center_y':0.75, 'center_x':0.5}
 
     MDLabel:
         text: 'Danceability'
-        pos_hint: {'center_y':0.70}
+        pos_hint: {'center_y':0.68}
+
+    MDTextField:
+        id: input_2
+        hint_text: '(0.051307 - 0.961871)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.68, 'center_x':0.5}
 
     MDLabel:
         text: 'Energy'
-        pos_hint: {'center_y':0.65}
+        pos_hint: {'center_y':0.61}
+
+    MDTextField:
+        id: input_3
+        hint_text: '(0.000279 - 0.999768)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.61, 'center_x':0.5}
 
     MDLabel:
         text: 'Instrumentalness'
-        pos_hint: {'center_y':0.6}
+        pos_hint: {'center_y':0.54}
+
+    MDTextField:
+        id: input_4
+        hint_text: '(0 - 0.993134)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.54, 'center_x':0.5}
 
     MDLabel:
         text: 'Liveness'
-        pos_hint: {'center_y':0.55}
+        pos_hint: {'center_y':0.47}
+
+    MDTextField:
+        id: input_5
+        hint_text: '(0.025297 - 0.971392)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.47, 'center_x':0.5}
 
     MDLabel:
         text: 'Speechiness'
-        pos_hint: {'center_y':0.50}
+        pos_hint: {'center_y':0.40}
+
+    MDTextField:
+        id: input_6
+        hint_text: '(0.023234 - 0.966177)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.40, 'center_x':0.5}
 
     MDLabel:
         text: 'Tempo'
-        pos_hint: {'center_y':0.45}
+        pos_hint: {'center_y':0.33}
+
+    MDTextField:
+        id: input_7
+        hint_text: '(29.093000 - 250.059000)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.33, 'center_x':0.5}
 
     MDLabel:
         text: 'Valence'
-        pos_hint: {'center_y':0.40}
+        pos_hint: {'center_y':0.26}
+
+    MDTextField:
+        id: input_8
+        hint_text: '(0.014392 - 0.983649)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.26, 'center_x':0.5}
+
+    MDLabel:
+        pos_hint: {'center_x':0.5, 'center_y':0.2}
+        text: ''
+        id: output_text
 
     MDRaisedButton:
         pos_hint: {'center_x':0.5, 'center_y':0.1}
