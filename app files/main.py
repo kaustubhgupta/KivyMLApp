546c0d542e8a283d95c253b753c051a05b11e9c9
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-10-31 23:40:42 +0530

    some changes

--- app files/main.py
+++ app files/main.py
@@ -13,30 +13,51 @@ ScreenManager:
     MDLabel:
         text: 'Music Genre Predictor'
         halign: 'center'
-        pos_hint: {'center_y':0.8}
-        font_style: 'H2'
+        pos_hint: {'center_y':0.9}
+        font_style: 'H3'
 
-    MDBoxLayout:
+    MDLabel:
+        text: 'Acousticness'
+        pos_hint: {'center_y':0.75}
 
-        MDSlider:
-            pos_hint: {'center_y':0.7}
-            halign: 'center'
-            min: 0.0000009
-            max: 0.0001
-            value: 0.0001
-            hint: False
-            id: slide_value
+    MDTextField:
+        hint_text: '(0.0000009491-0.99579650)'
+        width: 100
+        size_hint_x: None
+        pos_hint: {'center_y':0.75, 'center_x':0.5}
 
-        MDLabel:
-            text: ''
-            halign: 'center'
-            pos_hint: {'center_y':0.5}
-            id: sample_text
+    MDLabel:
+        text: 'Danceability'
+        pos_hint: {'center_y':0.70}
 
-        MDRaisedButton:
-            pos_hint: {'center_x':0.5, 'center_y':0.1}
-            text: 'Predict'
-            on_press: app.predict()
+    MDLabel:
+        text: 'Energy'
+        pos_hint: {'center_y':0.65}
+
+    MDLabel:
+        text: 'Instrumentalness'
+        pos_hint: {'center_y':0.6}
+
+    MDLabel:
+        text: 'Liveness'
+        pos_hint: {'center_y':0.55}
+
+    MDLabel:
+        text: 'Speechiness'
+        pos_hint: {'center_y':0.50}
+
+    MDLabel:
+        text: 'Tempo'
+        pos_hint: {'center_y':0.45}
+
+    MDLabel:
+        text: 'Valence'
+        pos_hint: {'center_y':0.40}
+
+    MDRaisedButton:
+        pos_hint: {'center_x':0.5, 'center_y':0.1}
+        text: 'Predict'
+        on_press: app.predict()
 '''
 
 class Main(Screen):
@@ -51,7 +72,9 @@ class MainApp(MDApp):
         return self.help_string
 
     def predict(self):
-        self.help_string.get_screen('main').ids.sample_text.text = str(self.help_string.get_screen('main').ids.slide_value.value)
+        self.help_string.get_screen('main').ids.sample_text_1.text = str(self.help_string.get_screen('main').ids.slide_value_1.value)
+        self.help_string.get_screen('main').ids.sample_text_2.text = str(self.help_string.get_screen('main').ids.slide_value_2.value)
+
 MainApp().run()
 
 # https://kivymlapp.herokuapp.com/predict?acousticness=0.344719513&danceability=0.758067547&energy=0.323318405&instrumentalness=0.0166768347&liveness=0.0856723112&speechiness=0.0306624283&tempo=101.993&valence=0.443876228
